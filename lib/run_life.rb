6a58f13bd23b90440fe43d92229ec1b0fd25def2
GeoffPurdy <geoff_purdy@yahoo.com>
2014-09-26 12:30:44 -0400

    added user prompt

--- lib/run_life.rb
+++ lib/run_life.rb
@@ -1,10 +1,11 @@
 require_relative "conway_life.rb"
 
-l = ConwayLife.new(8,1)
+l = ConwayLife.new(8,0)
 l.setCell(4,4,1)
 l.setCell(3,3,1)
 l.setCell(5,5,1)
 puts l.to_s
+puts "press [enter] to iterate; 'q' [enter] to exit"
 while(gets.chomp != 'q')
   l.sweep_flag_reseed
   puts l.to_s
